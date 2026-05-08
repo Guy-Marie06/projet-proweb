808904719c323ff6c525db9ad47d846006500bac
Guy-Marie06 <guymariecapochichi@gmail.com>
2026-05-08 22:03:08 +0100

    Ajout du code de la strucuture HTML principale

--- index.php
+++ index.php
@@ -1,1 +1,80 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Guy-Marie</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h2>Guy-Marie M.C.</h2>
+    <ul>
+        <li><a href="#A propos de moi">A propos de moi</a></li>
+        <li><a href="#Mes projets">Mes projets</a></li>
+        <li><a href="#Compétences">Compétences</a></li>
+        <li><a href="#Contact">Contact</a></li>
+    </ul>
 
+    <div id="A propos de moi">
+        <h2>A propos de moi</h2>
+        <div class="profil">
+            <img src="images/Avatar.PNG" alt="Avatar">
+            <div class="description"> <p>Jeune développeur et designer, passionné par son métier. Son ipséité réside en son obsession pour le minimalisme et la qualité visuelle exceptionnelle de ses conceptions. Ses idéaux sont la simplicité et l'efficacité.</p>
+            </div>
+        </div>
+           
+       
+    </div>
+
+    <div id="Mes projets">
+        <h2>Mes projets</h2>
+        <div class="projets-container">
+
+            <div class="projet">
+                <img src="images/Projet1.PNG" alt="Image du projet">
+                <h3>E-com Phones</h3>
+                <p>Il s'agit d'un site e-commerce au design sobre et élégant, destiné à la vente de téléphones portables et accessoires adéquats.</p>
+                <button><a href="">Voir plus</a></button>
+            </div>
+
+            <div class="projet">
+                <img src="images/Projet2.PNG" alt="Image du projet">
+                <h3>Chat With Me</h3>
+                <p>Ce projet illustre le développement et le déploiement d'un chatbot IA spécialisé dans la gestion automatique des négociations par message avec les clients.</p>
+                <button><a href="">Voir plus</a></button>
+            </div>
+
+            <div class="projet">
+                <img src="images/Projet3.PNG" alt="Image du projet">
+                <h3>Build Your Design</h3>
+                <p>Projet de maquette interactive avec design personnalisé.</p>
+                <button><a href="">Voir plus</a></button>
+            </div>
+
+        </div>
+    </div>
+
+    <div id="Compétences">
+        <div class="comp">
+            <h2>Mes compétences</h2>
+        <ul>
+            <li>Hard Skills</li>
+            <p>Maîtrise des logiciels Adobe (Photoshop, Illustrator, InDesign, XD)</p>
+            <li>Soft Skills</li>
+            <p>Esprit d'équipe, Sens de l'organisation, Créativité, Esprit critique</p>
+            <li>Compétences transversales</li>
+            <p>Gestion de projet, Rédaction de contenu, Analyse de données (Excel, Google)</p>
+        </ul>
+        </div>     
+    </div>
+
+    
+    <div id="Contact">
+        <h2>Contactez-moi</h2>
+        <a href="guymariecapochichi@gmail.com">Adresse Email</a> <br> <br>
+        <a href="https://github.com/Projet">GitHub</a>
+    </div>
+</body>
+
+<footer>&copy; 2025 - Guy-Marie</footer>
+</html>
